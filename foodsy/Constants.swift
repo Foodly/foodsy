593c8b8e5559388cb7ef237fbef7ce769ccdc1a3
Himal Sherchan <hsherchan@hearsaycorp.com>
2017-10-10 02:45:35 -0700

    Add user profile raw interaction

---
+++ foodsy/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  foodsy
+//
+//  Created by hsherchan on 10/10/17.
+//  Copyright © 2017 Foodly. All rights reserved.
+//
+
+import Foundation
+
+enum FoodPreferences : Int {
+    case RED = 1;
+    case YELLOW = 2;
+    case GREEN = 3;
+}
